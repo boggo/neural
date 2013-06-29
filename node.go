b58fddf9c4f806ad0fb6bf0511cc84750cda33f9
Scott H <github@cowszero.com>
2013-06-29 21:51:19 +0200

    Added Steepened Sigmoid
    Added the Steepened Sigmoid activation function and constant

--- node.go
+++ node.go
@@ -21,9 +21,10 @@ type FuncType byte
 const (
     DIRECT FuncType = iota
     SIGMOID
+	STEEPENED_SIGMOID
 )
 
-// Node interface 
+// Node interface
 type Node interface {
     Reset()
     Combine(value float64)
@@ -38,7 +39,7 @@ type nodeList []Node
 // Len is the number of Nodes in the nodeList. Part of sort.Interface
 func (nl nodeList) Len() int { return len(nl) }
 
-// Less returns whether the Node with index i in the nodeList should 
+// Less returns whether the Node with index i in the nodeList should
 // sort before Node with index j. Part of sort.Interface
 func (nl nodeList) Less(i, j int) bool { return nl[i].NodeType() < nl[j].NodeType() }
 
@@ -49,7 +50,7 @@ func (nl nodeList) Swap(i, j int) { nl[i], nl[j] = nl[j], nl[i] }
 type node struct {
     input    float64
     nodeType NodeType
-	funcType FuncType
+    funcType FuncType
 }
 
 // Reset a node to its starting value, 0. For Bias nodes, this is 1.
@@ -63,7 +64,7 @@ func (n *node) Reset() {
 
 // Combines the new value with the existing input value of the Node. For Bias
 // nodes this throws an error. For input nodes, this just replaces the input value.
-// For all other Nodes, this adds the new value to the input value.	
+// For all other Nodes, this adds the new value to the input value.
 func (n *node) Combine(value float64) {
     switch n.NodeType() {
     case BIAS:
@@ -93,9 +94,9 @@ func NewNode(funcType FuncType, nodeType NodeType) Node {
     case SIGMOID:
         return NewSigmoidNode(nodeType)
     }
-	
-	// Unknown FuncType, return nil
-	return nil
+
+    // Unknown FuncType, return nil
+    return nil
 }
 
 // DirectNode is an implemenation of Node which returns its input value without
@@ -128,8 +129,28 @@ func NewSigmoidNode(nodeType NodeType) *SigmoidNode {
 // Activate returns the input value transformed by the Sigmoid function:
 // by the sigmoid function:    1
 //                          -------
-//                               -t     
+//                               -t
 //                          1 + e
 func (n SigmoidNode) Activate() float64 {
     return 1.0 / (1.0 + math.Pow(math.E, -n.input))
+}
+
+// SigmoidNode is an implementation of Node which returns its input value transformed
+// by the sigmoid function.
+type SteepenedSigmoidNode struct {
+    node
+}
+
+// NewSigmoidNode returns a pointer to a new Sigmoid Node
+func NewSteepenedSigmoidNode(nodeType NodeType) *SteepenedSigmoidNode {
+    return &SteepenedSigmoidNode{node{nodeType: nodeType}}
+}
+
+// Activate returns the input value transformed by the Sigmoid function:
+// by the sigmoid function:    1
+//                          -------
+//                               -4.9t
+//                          1 + e
+func (n SteepenedSigmoidNode) Activate() float64 {
+    return 1.0 / (1.0 + math.Pow(math.E, -4.9*n.input))
 }
